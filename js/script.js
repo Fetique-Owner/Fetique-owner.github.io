1ae29b8a9b330437fa278746085f147b15fb7255
Fetique | Сoming soon <228181855+Fetique-Owner@users.noreply.github.com>
2025-09-05 22:36:00 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,2 @@
+window.addEventListener('load', function () {setTimeout(function () {const preloader = document.querySelector('.preloader');preloader.classList.add('hidden-preloader');}, 1500);});
+
